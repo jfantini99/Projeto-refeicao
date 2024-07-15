066e274630d67e84594433c496ab0bf05591779b
jfantini99 <jfantini99@gmail.com>
2024-07-15 16:54:56 -0300

    feat novas pastas

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,30 @@
+import classes from './page.module.css';
+import Link from 'next/link';
+
 export default function MealsPage() {
     return (
-        <div>
-        <h1>Meals Page</h1>
-        </div>
+        <>
+        <header className={classes.header}>
+         <h1>refeições deliciosas, criadas{''}
+         <span className={classes.highlight}> para você </span>
+         </h1>
+         <p> como escolher sua comida favorita e fazer você mesmo. É facil e divertido!  </p>
+         <p className={classes.cta}>
+            <Link href='/meals/share'>
+            Compartilhe sua receita
+            </Link>
+         </p>
+        </header>
+
+        <main className={classes.main}> 
+          
+
+    
+
+        </main>
+        
+        
+        
+        </>
     );
     }
---
+++ components/meals/meals-grid.js
@@ -0,0 +1,8 @@
+import classes from './meals-grid.module.css';
+
+export default function MealsGrid(meals) {
+  return 
+  <ul className={classes.meals}>
+    {meals.map((meals) => <li key={meal.id}> </li>)}
+  </ul>
+}
